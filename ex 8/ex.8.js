f41349b62b598601d3c9df4c4c6bd1b74a2f6f4d
RuphiaYen <113159766+RuphiaYen@users.noreply.github.com>
2022-11-06 12:52:45 +0800

    update

---
+++ ex 8/ex.8.js
@@ -0,0 +1,9 @@
+function changeText() {
+document.getElementById('change-button').innerText = 'Done';
+}
+
+
+function changeOtherText() {
+  let button = document.getElementById('change2-button');
+  button.innerText = 'Changed';
+}
