f7a474494fccdad3c0ae59bd74ef92f684e93bf0
RuphiaYen <113159766+RuphiaYen@users.noreply.github.com>
2022-11-10 18:00:34 +0800

    Create Ex.10.1.js

---
+++ Exercise 10/Ex.10.1.js
@@ -0,0 +1,12 @@
+function cartTotal(cartArray) {
+  let total = 0
+  
+  cartArray.forEach(item => total = total + item.price * item.quantity
+  )
+  console.log(total)
+}
+
+cartTotal([
+  { name: 'Apple', price: 4, quantity: 2 },
+  { name: 'Orange', price: 3, quantity: 3 }
+]);
