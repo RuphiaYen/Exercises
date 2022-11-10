679f69226bd6a5cdb4f366e96b7924174495f179
RuphiaYen <113159766+RuphiaYen@users.noreply.github.com>
2022-11-10 12:19:28 +0800

    Update Ex.9.2.js

--- Exercise 9/Ex.9.2.js
+++ Exercise 9/Ex.9.2.js
@@ -1,1 +1,23 @@
+let cmBtn = document.querySelector('.cm')
+let inchBtn = document.querySelector('.inch')
+let input = document.querySelector('input')
+
+cmBtn.addEventListener('click', convertToCm)
+inchBtn.addEventListener('click', convertToInch)
+
+function convertToCm() {
+  const value = input.value
+  const convertedValue = value * 2.54;
+  const result = document.createElement('div')
+  result.innerText = convertedValue
+  document.body.appendChild(result)
+}
+
+function convertToInch() {
+  const value = input.value
+  const convertedValue = value / 2.54
+  const result = document.createElement('div')
+  result.innerText = convertedValue
+  document.body.appendChild(result)
+}
 
